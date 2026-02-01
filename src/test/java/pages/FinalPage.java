18fa72bcf5e707a09f1b0b6f2bd02c88516faa74
ekostyukova <kostyukova_ea@mail.ru>
2026-02-01 14:26:59 +0500

    fix8

--- src/test/java/pages/FinalPage.java
+++ src/test/java/pages/FinalPage.java
@@ -1,47 +1,23 @@
 package pages;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class FinalPage {
-
-    private final SelenideElement popupWrapper = $("._close_1b9nk_55 popup__close close closeWhite--MwvwP");
-    private final SelenideElement searchInput = $(".search-catalog__block search-catalog--active");
 
     private final SelenideElement cookieWindow = $(".Tooltip_root__EMk_3 Tooltip_whiteTheme__bkjSb");
     private final SelenideElement cookieButton = $(".Tooltip_closeIcon__skwl0");
     private final SelenideElement inputSearch = $(".UiSharedInputSearch_input__G0Un1");
     private final SelenideElement sharedInputSearch = $(".UiSharedInputSearch_search__zN2I2");
-
-
-
+    private final SelenideElement addingProduct = $(".Purchase_cartBtn__POyOa");
 
     public FinalPage openPage(String address) {
         open(address);
         return this;
     }
-
-//    public FinalPage popupWrapper() {
-//        popupWrapper.find(String.valueOf(Condition.exist));
-//        if (popupWrapper.exists()) {
-//            popupWrapper.click();
-//        }
-//        return this;
-//    }
-//
-//    public FinalPage setSearchInput(){
-//        searchInput.shouldBe(visible).click();
-//        return this;
-//    }
 
     public FinalPage acceptCookies() {
         if (cookieWindow.is(visible)) {
@@ -66,7 +42,10 @@ public class FinalPage {
         return this;
     }
 
-
+    public FinalPage setAddingProduct() {
+        addingProduct.shouldBe(visible).click();
+        return this;
+    }
 
 }
 
--- src/test/java/pages/FinalPageTest.java
+++ src/test/java/pages/FinalPageTest.java
@@ -1,59 +1,22 @@
 package pages;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.*;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ExtendWith;
-import io.qameta.allure.selenide.AllureSelenide;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static org.assertj.core.api.Assertions.assertThat;
-
-import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
 
-
-
-@Tag("wildberries")
+@Tag("tests")
 public class FinalPageTest extends TestBase {
 
     FinalPage finalPage = new FinalPage();
     ResultComponent resultComponent = new ResultComponent();
 
-//    @Test
-//    @DisplayName("Добавление товара в корзину и проверка наличия товара в корзине")
-//    void siteSearch() {
-//        step("Открываем главную страницу", () -> {
-//            finalPage.openPage("https://www.wildberries.ru/");
-//        });
-//
-//        step("Поиск и закрытие всплывающего банера", () -> {
-//            finalPage.popupWrapper();
-//        });
-//
-//        step("Кликаем по поисковой строке", () -> {
-//            finalPage.setSearchInput();
-//        });
-//
-//
-//    }
-
-
     @Test
     @DisplayName("Проверка поиска товара на сайте")
-    void siteSearch1() {
+    void productSearch() {
         step("Открываем главную страницу", () -> {
             finalPage.openPage("https://www.vprok.ru/");
         });
@@ -81,40 +44,37 @@ public class FinalPageTest extends TestBase {
         step("Проверяем результат", () -> {
             resultComponent.findInCards("Сливки");
         });
-
     }
 
+    @Test
+    @DisplayName("Добавление товара в корзину")
+    void AddingAnItemToTheCart() {
+        step("Открываем главную страницу", () -> {
+            finalPage.openPage("https://www.vprok.ru/");
+        });
 
-//    @ValueSource(strings = {
-//            "Растущий стул",
-//            "Куртка зимняя женская"})
-//    @ParameterizedTest(name = "Для поискового запроса {0} должен отдавать не пустой список карточек")
-//    void searchResultsShouldNotBeEmpty1(String searchQuery) {
-//
-//        step("Открываем главную страницу", () -> {
-//            finalPage.openPage("https://www.wildberries.ru/");
-//        });
-//
-//        step("Поиск и закрытие всплывающего банера", () -> {
-//            finalPage.popupWrapper();
-//        });
-//
-//        step("Поиск по запросу", () -> {
-//            finalPage.setValue(searchQuery);
-//            finalPage.pressEnter();
-//        });
-//
-//        step("Проверка, что коллекция содержит хотя бы один элемент", () -> {
-//            finalPage.shouldBe();
-//        });
-//    }
+        step("Закрываем банер с куки", () -> {
+            finalPage.acceptCookies();
+        });
 
+        step("Кликаем по полю поиска", () -> {
+            finalPage.setInputSearch();
+        });
 
+        step("Вводим слово для поиска Шоколад", () -> {
+            finalPage.sharedInputSearch("Шоколад");
+        });
 
+        step("Кликаем по кнопке Найти", () -> {
+            finalPage.setSharedInputSearch();
+        });
 
+        step("Добавляем товар в корзину", () -> {
+            finalPage.setAddingProduct();
+        });
 
 
-
+    }
 }
 
 
